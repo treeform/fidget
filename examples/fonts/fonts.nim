2891fdc55336d4ea697253fef178e87705f4d841
treeform <starplant@gmail.com>
2019-02-25 10:33:03 -0800

    Add window obj, with title and path. Also cairo backend.

---
+++ examples/fonts/fonts.nim
@@ -0,0 +1,86 @@
+import ../../src/fidget
+
+import print
+import random
+import math
+
+
+var bars = newSeq[int](1000)
+for i, bar in bars:
+  bars[i] = rand(40)
+
+
+drawMain = proc() =
+
+  window.title = "Fidget Fonts Example"
+
+  let h = bars.len * 60 + 20
+  frame "main":
+    box 0, 0, int root.box.w, max(int root.box.h, h)
+    rectangle "#F7F7F9"
+
+    text "t":
+      box 10, 10, 300, 50
+      fill "#46D15F"
+      font "Sans", 50, 200, 16, -1, -1
+      characters "Font Features"
+
+    group "box":
+      box 100, 100, 300, 300
+      fill "#AEB5C0"
+
+      text "tl":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, -1, -1
+        characters "top left"
+
+      text "tr":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 1, -1
+        characters "top right"
+
+      text "tm":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 0, -1
+        characters "top middle"
+
+      text "cl":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, -1, 0
+        characters "center left"
+
+      text "cr":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 1, 0
+        characters "center right"
+
+      text "cm":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 0, 0
+        characters "center middle"
+
+      text "bl":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, -1, 1
+        characters "bottom left"
+
+      text "br":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 1, 1
+        characters "bottom right"
+
+      text "bm":
+        box 0, 0, 300, 300
+        fill "#FFFFFF"
+        font "Sans", 12, 200, 16, 0, 1
+        characters "bottom middle"
+
+startFidget()
