3eb4af51adfe3390a848b1e4ec65e56073e50972
Andre von Houck <starplant@gmail.com>
2019-05-02 16:34:17 -0700

    Add hovers example.

---
+++ examples/hovers/hovers.nim
@@ -0,0 +1,39 @@
+import ../../src/fidget
+
+import print
+import random
+import vmath
+
+
+var bars = newSeq[int](10)
+for i, bar in bars:
+  bars[i] = rand(40)
+
+
+drawMain = proc() =
+
+  window.title = "Hovers Example"
+
+  popupActive = true
+
+  let h = bars.len * 60 + 20
+  frame "main":
+    box 0, 0, int root.box.w, max(int root.box.h, h)
+
+    group "center":
+      box 100, 100, 100, 100
+      fill "#0000FF"
+
+      onHover:
+        fill "#FF0000"
+       
+    inPopup = true
+    group "center":
+      box 150, 150, 100, 100
+      fill "#00FF00"
+      
+      onHover:
+        fill "#FF0000"
+    inPopup = false
+
+startFidget()
