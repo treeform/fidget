d794cc0428157b67479dbc8a571784ff5c3cc247
treeform <starplant@gmail.com>
2019-05-10 09:21:57 -0700

    Add padoftext example and some new fonts.

--- src/fidget/backendopengl.nim
+++ src/fidget/backendopengl.nim
@@ -1,211 +1,217 @@
-import typography, chroma
-# TODO: fix dependencies
-include ../../../engine/all
-import ../../../client/common
+## Cairo backend uses cairo and glfw3 libarires to provide graphics and input
 
+import uibase, times
 
-export color, print, HAlignMode, VAlignMode
+import glfw3 as glfw, math, vmath, opengl, chroma, print, os
+import random
 
-type
-  NodeKind* = enum
-    Frame
-    Rectangle
-    Group
-    Text
-    Instance
-
-  Node* = object
-    name*: string
-    box*: Rect
-    kind*: NodeKind
-    fillColor*: Color
-
-    textAlignHorizontal: HAlignMode
-    textAlignVertical: VAlignMode
-    fontFamily: string
-    fontSize: float32
-    textLineHeight: float32
-
-
-var nodeStack = newSeq[Node](0)
 
 var
-  current*: ptr Node
-  parent*: ptr Node
+  # surface: Surface
+  # ctx: Context
+  frameCount = 0
+  window: glfw.Window
+  dpi*: float = 1.0
+  windowFrame: Box
+  viewPort: Box
 
 
-proc pushStack(kind: NodeKind, name: string) =
-  # draw everything about parent node
+proc draw*(group: Group) =
+  ## Redraws the whole screen
+  if group.fill.a > 0:
 
-  nodeStack.add Node()
-  nodeStack[^1].kind = kind
-  nodeStack[^1].name = name
+    if group.kind == "text":
+      if group.text.len > 0:
+        glBegin(GL_QUADS)
+        glColor4f(group.fill.r, group.fill.g, group.fill.b, group.fill.a)
+        proc drawRect(sx, sy, sw, sh: float) =
+          let
+            wf = windowFrame
+            x = sx / wf.w*2 - 1.0
+            y = -sy / wf.h*2 + 1.0
+            w = sw / wf.w*2
+            h = -sh / wf.h*2
+          glVertex2d(x,     y + h)
+          glVertex2d(x + w, y + h)
+          glVertex2d(x + w, y)
+          glVertex2d(x,     y)
+        let b = group.screenBox
+        drawRect(b.x, b.y, b.w, b.h)
+        glEnd()
+        # ctx.selectFontFace(group.textStyle.fontFamily, FONT_SLANT.normal, FONT_WEIGHT.normal)
+        # ctx.setFontSize(group.textStyle.fontSize)
+        # ctx.setSource(group.fill)
+        # var extents = TextExtents()
+        # ctx.textExtents(group.text, extents)
+        # var x, y: float
+        # case group.textStyle.textAlignHorizontal:
+        #   of -1:
+        #     x = group.screenBox.x
+        #   of 0:
+        #     x = group.screenBox.x + group.screenBox.w/2 - float(extents.width)/2
+        #   of 1:
+        #     x = group.screenBox.x + group.screenBox.w - extents.width
 
-  current = addr nodeStack[^1]
-  if nodeStack.len > 1:
-    parent = addr nodeStack[^2]
+        # case group.textStyle.textAlignVertical:
+        #   of -1:
+        #     y = group.screenBox.y + extents.height
+        #   of 0:
+        #     y = group.screenBox.y + group.screenBox.h/2 - float(extents.height)/2
+        #   of 1:
+        #     y = group.screenBox.y + group.screenBox.h
 
-proc popStack() =
-  discard nodeStack.pop()
-
-  if nodeStack.len > 0:
-    current = addr nodeStack[^1]
-  if nodeStack.len > 1:
-    parent = addr nodeStack[^2]
-
-template frame*(name: string, body: untyped) =
-  ## A frame node
-  nodeStack.setLen(0)
-
-  pushStack(Frame, name)
-
-  block:
-    body
-
-  popStack()
-
-template group*(name: string, body: untyped) =
-  ## A frame node
-  pushStack(Group, name)
-  block:
-    body
-
-  popStack()
-
-template rectangle*(name: string, body: untyped) =
-  ## A rectangle node
-  pushStack(Rectangle, name)
-
-  block:
-    body
-
-  popStack()
-
-template text*(name: string, body: untyped) =
-  ## A rectangle node
-  pushStack(Text, name)
-  nodeStack[^1].textAlignHorizontal = HAlignMode.Left
-  nodeStack[^1].textAlignVertical = VAlignMode.Top
-
-  block:
-    body
-
-  popStack()
-
-
-template component*(name: string, body: untyped) =
-  ## A component node
-  pushStack(Instance, name)
-
-  block:
-    body
-
-  popStack()
+        # ctx.moveTo(x, y)
+        # ctx.showText(group.text)
+    else:
+      glBegin(GL_QUADS)
+      glColor4f(group.fill.r, group.fill.g, group.fill.b, group.fill.a)
+      proc drawRect(sx, sy, sw, sh: float) =
+        let
+          wf = windowFrame
+          x = sx / wf.w*2 - 1.0
+          y = -sy / wf.h*2 + 1.0
+          w = sw / wf.w*2
+          h = -sh / wf.h*2
+        glVertex2d(x,     y + h)
+        glVertex2d(x + w, y + h)
+        glVertex2d(x + w, y)
+        glVertex2d(x,     y)
+      let b = group.screenBox
+      drawRect(b.x, b.y, b.w, b.h)
+      glEnd()
 
 
-template instance*(name: string, body: untyped) =
-  ## A instance node
-  pushStack(Instance, name)
-
-  block:
-    body
-
-  popStack()
+proc redraw*() =
+  ## Request the screen to be redrawn next
+  if not requestedFrame:
+    requestedFrame = true
 
 
-template click*(body: untyped) =
-  ## What to do when user clicks
-  if current.box.intersects(mousePos):
-    let darkBg = color(0, 0, 0, 0.50)
-    ctx.drawImageRect("data/ui/selection.png", at=current.box.xy, to=xy(current.box) + wh(current.box), color=darkBg, marginIn=1)
-    if buttonPress[MOUSE_LEFT] == true:
-      body
-
-proc componentId*(componentId: string) =
-  ## sets the compoenet id for instance node
+proc openBrowser*(url: string) =
+  ## Opens a URL in a browser
+  discard
 
 
-proc image*(imagePath: string) =
-  ## draws an image
-  let b = nodeStack[^1].box
-  if nodeStack[^1].fillColor == color(0,0,0,0):
-    nodeStack[^1].fillColor = color(1,1,1,1)
-
-  var at = vec2(b.x, b.y)
-  var to = vec2(b.x + b.w, b.y + b.h)
-
-  ctx.drawImageRect(
-    "data/ui/" & imagePath & ".png",
-    at=vec2(b.x, b.y),
-    to=vec2(b.x + b.w, b.y + b.h),
-    color = nodeStack[^1].fillColor,
-    marginIn = 0
-  )
+proc goto*(url: string) =
+  ## Goes to a new URL, inserts it into history so that back button works
+  rootUrl = url
+  redraw()
 
 
-proc fill*(color: Color) =
-  nodeStack[^1].fillColor = color
+proc display() =
+  echo "display"
+  ## Called every frame by main while loop
 
-proc textAlignHorizontal*(mode: HAlignMode) =
-  nodeStack[^1].textAlignHorizontal = mode
+  setupRoot()
 
-proc textAlignVertical*(mode: VAlignMode) =
-  nodeStack[^1].textAlignVertical = mode
+  root.box.x = float 0
+  root.box.y = float 0
+  root.box.w = windowFrame.w
+  root.box.h = windowFrame.h
 
-proc fontFamily*(fontFamily: string) =
-  nodeStack[^1].fontFamily = fontFamily
+  scrollBox.x = float 0
+  scrollBox.y = float 0
+  scrollBox.w = root.box.w
+  scrollBox.h = root.box.h
 
-proc fontSize*(fontSize: float) =
-  nodeStack[^1].fontSize = fontSize
+  drawMain()
 
-proc textLineHeight*(textLineHeight: float) =
-  nodeStack[^1].textLineHeight = textLineHeight
+  # # update texture with new pixels from surface
+  # let
+  #   dataPtr = surface.imageSurfaceGetData()
+  #   w = surface.width
+  #   h = surface.height
+  # glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, GLsizei w, GLsizei h, GL_RGBA, GL_UNSIGNED_BYTE, dataPtr)
 
-proc font*(fontFamily: string, fontSize, textLineHeight: float, textAlignHorizontal, textAlignVertical: int) =
-  nodeStack[^1].fontFamily = fontFamily
-  nodeStack[^1].fontSize = fontSize
-  nodeStack[^1].textLineHeight = textLineHeight
-  nodeStack[^1].textAlignHorizontal =
-    [HAlignMode.Left, HAlignMode.Center, HAlignMode.Right][textAlignHorizontal + 1]
-  nodeStack[^1].textAlignVertical =
-    [VAlignMode.Top, VAlignMode.Middle, VAlignMode.Bottom][textAlignVertical + 1]
 
-proc characters*(text: string) =
-  let node = nodeStack[^1]
-  var font: Font
-  if node.fontFamily == "Moon":
-    font = moonFont
+
+  inc frameCount
+  glfw.SwapBuffers(window)
+
+
+proc closestPowerOf2(v: int): int =
+  ## returns closets power of 2 ... 2,4,8,16... that is higher then v
+  result = 2
+  while true:
+    if v < result:
+      return
+    result *= 2
+
+
+proc resize() =
+  var cwidth, cheight: cint
+  GetWindowSize(window, addr cwidth, addr cheight)
+  windowFrame.w = float(cwidth)
+  windowFrame.h = float(cheight)
+
+  GetFramebufferSize(window, addr cwidth, addr cheight)
+  viewPort.w = float(cwidth)
+  viewPort.h = float(cheight)
+  dpi = viewPort.w / windowFrame.w
+  glViewport(0, 0, cwidth, cheight)
+
+
+proc onResize(handle: glfw.Window, w, h: int32) {.cdecl.} =
+  resize()
+  display()
+
+
+proc onMouseButton(window: glfw.Window, button: cint, action: cint, modifiers: cint) {.cdecl.} =
+  if action == 0:
+    mouse.down = false
+    mouse.click = false
   else:
-    font = scoutFont
+    mouse.click = true
+    mouse.down = true
+  # let button = button + 1
+  # if button < buttonDown.len:
+  #   if buttonDown[button] == false and setKey == true:
+  #     buttonPress[button] = true
+  #   buttonDown[button] = setKey
+  redraw()
 
-  ctx.simpleText(
-    font,
-    pos = node.box.xy,
-    size = node.box.wh,
-    text = text,
-    fontSize = node.fontSize,
-    hAlign = node.textAlignHorizontal,
-    vAlign = node.textAlignVertical,
-    color = node.fillColor,
-    lineHeight = node.textLineHeight
-  )
+proc onMouseMove(window: glfw.Window, x: cdouble, y: cdouble) {.cdecl.} =
+  # this does not fire when mouse is not in the window
+  mouse.pos = vec2(x, y) * dpi
+  redraw()
 
-proc box*(x, y, w, h: float) =
-  ## draws a ui box
-  current.box.x = floor(x)
-  current.box.y = floor(y)
-  current.box.w = floor(w)
-  current.box.h = floor(h)
+proc `title=`*(win: uibase.Window, title: string) =
+  if win.innerTitle != title:
+    win.innerTitle = title
+    window.SetWindowTitle(title)
 
-  if current.kind == Frame:
-    current.box.x = 0.0
-    current.box.y = 0.0
-    current.box.w = windowFrame.x
-    current.box.h = windowFrame.y
-  else:
-    current.box.x += parent.box.x
-    current.box.y += parent.box.y
 
-  if current.kind == Rectangle:
-    let b = nodeStack[^1].box
-    uiRect(b.x, b.y, b.w, b.h, current.fillColor)
+proc `title`*(win: uibase.Window): string =
+  win.innerTitle
+
+
+proc startFidget*() =
+  ## Starts cairo backend
+  if glfw.Init() == 0:
+    quit("Failed to Initialize GLFW")
+  window = glfw.CreateWindow(1000, 800, "Fidget glfw/cairo backend window.", nil, nil)
+  glfw.MakeContextCurrent(window)
+  loadExtensions()
+
+  glfw.PollEvents()
+  resize()
+
+  glDisable(GL_CULL_FACE)
+
+  discard SetCursorPosCallback(window, onMouseMove)
+  discard SetMouseButtonCallback(window, onMouseButton)
+  discard SetFramebufferSizeCallback(window, onResize)
+
+  requestedFrame = true
+
+  while glfw.WindowShouldClose(window) == 0:
+    glfw.PollEvents()
+
+    if requestedFrame:
+      requestedFrame = false
+      display()
+    else:
+      sleep(1)
+
+    # reset one off events
+    mouse.click = false
