739faedd6325dd3c9d01f8ee9effb1aff0e9c612
Ryan Oldenburg <guzba8@gmail.com>
2020-03-10 21:17:34 -0500

    morepretty, remove ;

--- src/fidget/opengl/basemobile.nim
+++ src/fidget/opengl/basemobile.nim
@@ -94,7 +94,7 @@ type
 
 var app: ExampleApp
 
-proc compileShader*(`type`: GLenum; shaderString: string): GLuint =
+proc compileShader*(`type`: GLenum, shaderString: string): GLuint =
   var shader: GLuint = glCreateShader(`type`)
   var shaderTextArr = [cstring(shaderString)]
   echo "glShaderSource..."
@@ -121,7 +121,7 @@ proc compileShader*(`type`: GLenum; shaderString: string): GLuint =
   return shader
 
 var firstFrame = true
-proc onFrame*(display: ptr GLFMDisplay; frameTime: cdouble) {.exportc.} =
+proc onFrame*(display: ptr GLFMDisplay, frameTime: cdouble) {.exportc.} =
   # display screen going from black to white
 
   if firstFrame:
