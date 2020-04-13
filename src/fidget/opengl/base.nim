2068a1d180cd7ab90c9b5939a7b4c91cc815d8e5
Ryan Oldenburg <guzba8@gmail.com>
2020-04-12 21:00:02 -0500

    f

--- src/fidget/opengl/base.nim
+++ src/fidget/opengl/base.nim
@@ -365,10 +365,6 @@ proc start*(openglVersion: (int, int), msaa: MSAA, mainLoopMode: MainLoopMode) =
   discard window.setCursorPosCallback(onMouseMove)
   discard window.setCharCallback(onSetCharCallback)
 
-  # glEnable(GL_CULL_FACE)
-  # glCullFace(GL_BACK)
-  # glFrontFace(GL_CCW)
-
   glEnable(GL_BLEND)
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
