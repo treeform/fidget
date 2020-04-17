e467264c72ed882179f490ecba14ea64720f16bb
treeform <starplant@gmail.com>
2020-04-17 07:44:16 -0700

    f

--- src/fidget/opengl/shaders.nim
+++ src/fidget/opengl/shaders.nim
@@ -206,7 +206,6 @@ proc newShader*(computePath: string): Shader =
 template newShaderStatic*(computePath: string): Shader =
   ## Creates a new shader but also statically reads computePath
   ## So they are compiled into the binary.
-  ##
   const
     computeCode = staticRead(computePath)
     dir = currentSourcePath()
