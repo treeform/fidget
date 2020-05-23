ff05298f4ce911c3e0775abbc23f6944c95052dd
treeform <starplant@gmail.com>
2020-05-23 14:31:23 -0700

    Make masks work in HTML mode better.

--- tests/masks/masks.nim
+++ tests/masks/masks.nim
@@ -2,6 +2,9 @@ import fidget, math, random
 
 loadFont("IBM Plex Sans Regular", "../../examples/data/IBMPlexSans-Regular.ttf")
 
+
+var clip = true
+
 proc drawMain() =
 
   font "IBM Plex Sans Regular", 12, 200, 16, hLeft, vTop
@@ -23,19 +26,33 @@ proc drawMain() =
         box 50, 50, 100, 100
         fill "#00FF00"
 
+      text "childText":
+        box 50, 50, 100, 100
+        fill "#0000FF"
+        textAlign hLeft, vTop
+        characters "Some text here"
+
     text "noClipped":
       box 500, 80, 100, 20
       fill "#FF0000"
       textAlign hLeft, vTop
-      characters "Clipped"
+      characters "Clipped " & $clip
+      onClick:
+        clip = not clip
 
     group "clippedParent":
-      clipContent true
+      clipContent clip
       box 500, 100, 100, 100
       fill "#0000FF"
 
       group "clippedChild":
         box 50, 50, 100, 100
         fill "#00FF00"
+
+      text "childText":
+        box 50, 50, 100, 100
+        fill "#0000FF"
+        textAlign hLeft, vTop
+        characters "Some text here"
 
 startFidget(drawMain)
